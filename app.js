1ef7ca2172f4bf9cd3c7cd75667d1a7b53d5489b
Anthony Cera <ace_cera@yahoo.com>
2020-03-31 22:41:39 -0700

    installed express package and added a src and public directory.

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const url ='https://api.darksky.net/forecast/82031761e1a3fc016d1b0aa51740d02e/37.8267,-122.4233?units=us'
+const address = process.argv[2]
 
-request({ url: url, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to weather service!')
-    } else if (response.body.error) {
-        console.log('Unable to find location')
-    } else {
-        console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')   
-    }
-})
+if (!address) {
+    console.log('Please provide address!')
+} else {
+    geocode(address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return console.log(error)
+        } 
+    
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+    
+    
+            console.log(location)
+            console.log(forecastData)
+        })
+    })
+}
 
-const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW50aG9ueWNlcmEiLCJhIjoiY2s4YWpmNWFvMDFiNzNrcDdsbzRuY3EzYyJ9.s9bBTJ9krW9OZ_smPG_cdA'
-
-request({ url: geoUrl, json: true}, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to location services!')
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location.k Try another search.')
-    } else {
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(latitude, longitude)
-    }
-})
