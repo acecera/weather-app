45680aa459e07eb08ca623ae0c23bec719b8f925
Anthony Cera <ace_cera@yahoo.com>
2020-04-04 18:26:56 -0700

    added some styling. downloaded the hbs package. Created handlbars to add into the help page, 404 page, weather page and about page.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,64 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express configuration
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Ace'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Ace'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('404', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Ace'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 'It is sunny',
         location: 'Los Angeles'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Ace',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Ace',
+        errorMessage: 'Page not found'
     })
 })
 
