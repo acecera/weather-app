acfe149a6d87cf19199ba960f3c6bb51097ac612
Anthony Cera <ace_cera@yahoo.com>
2020-04-05 21:38:50 -0700

    addeded a limit value of 1 to the url.

--- utils/geocode.js
+++ utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYW50aG9ueWNlcmEiLCJhIjoiY2s4YWpmNWFvMDFiNzNrcDdsbzRuY3EzYyJ9.s9bBTJ9krW9OZ_smPG_cdA'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYW50aG9ueWNlcmEiLCJhIjoiY2s4YWpmNWFvMDFiNzNrcDdsbzRuY3EzYyJ9.s9bBTJ9krW9OZ_smPG_cdA&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
