c861a5235bfeb16ff736dc2c341259cc4c4a8737
Anthony Cera <ace_cera@yahoo.com>
2020-04-06 19:24:24 -0700

    added the geocode request and the forecast request in the weather page get.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('../src/utils/geocode')
+const forecast = require('../src/utils/forecast')
 
 const app = express()
 
@@ -46,11 +48,29 @@ app.get('/weather', (req, res) => {
         })
     }
 
-    res.send({
-        forecast: 'It is raining',
-        location: 'Los Angeles',
-        address: req.query.address
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
+
+    // res.send({
+    //     forecast: 'It is raining',
+    //     location: 'Los Angeles',
+    //     address: req.query.address
+    // })
 })
 
 app.get('/help/*', (req, res) => {
